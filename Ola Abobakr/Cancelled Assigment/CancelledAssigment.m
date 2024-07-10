f185eaf63d3133ff84469811931a73a54e4278df
Ola Abobakr <olaabobakr@gmail.com>
2024-07-10 18:03:19 +0300

    Done

---
+++ Ola Abobakr/Cancelled Assigment/CancelledAssigment.m
@@ -0,0 +1,16 @@
+clc
+clear
+grade=input("Please, Enter your grade ","s");
+if(grade=='A')
+    disp('Exellent!')
+elseif(grade=='B')
+    disp('very Good')
+elseif(grade=='C')
+    disp('Good')
+elseif(grade=='D')
+    disp('You Passed')
+elseif(grade=='F')
+    disp('Try Again')
+else
+    disp('Invalid Grade')
+end    
